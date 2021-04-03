3b5dd68af7b3d98c8c5ef63c35fb0af5ae40e164
KevinAngeles <kevin.angeles@outlook.com>
2021-04-02 20:38:22 -0500

    Convert header to sticky header on scroll. Add smooth scrolling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,85 @@
-import React from 'react';
-import { Route, Switch, NavLink } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react';
+import { Route, Switch, NavLink, Link } from 'react-router-dom';
 import './styles/app.scss';
 import braustintx from './images/braustintx.png';
 import Home from './components/Home/index';
 import About from './components/About';
 
 function App() {
+  const [stickyHeader, setStickyHeader] = useState(false);
+  const [stickyNavigation, setStickyNavigation] = useState(false);
+  const [stickyNavigationList, setStickyNavigationList] = useState(false);
+  const [stickyNavigationLink, setStickyNavigationLink] = useState(false);
+  const [stickyLogo, setStickyLogo] = useState(false);
+  let headerRef = useRef();
+  useEffect(() => {
+    const node = headerRef.current as any;
+    console.log(node);
+    //console.log(node.offsetTop); 
+    // const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+
+  }, [headerRef]);
+  // const handleScroll = (e:any) => {
+  //   console.log("hereee");
+  //   if(bottom) {
+  //     console.log("bottom reached");
+  //   } else {
+  //     console.log("normal");
+  //   }
+  // }
+
+  let eventTimeout: any | null = null;
+  const scrollHandler = (ev: any) => {
+    if (window.scrollY <= 80) {
+      setStickyHeader(false);
+      setStickyNavigation(false);
+      setStickyNavigationList(false);
+      setStickyNavigationLink(false);
+      setStickyLogo(false);
+    } else {
+      setStickyHeader(true);
+      setStickyNavigation(true);
+      setStickyNavigationList(true);
+      setStickyNavigationLink(true);
+      setStickyLogo(true);
+    }
+  }
+  const eventThrottler = (ev: any) => {
+    if (!eventTimeout) {
+      eventTimeout = setTimeout(() => {
+        eventTimeout = null;
+        scrollHandler(ev);
+      }, 250);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', eventThrottler);
+    return () => window.removeEventListener('scroll', eventThrottler);
+  }, []);
   return (
     <div className="container">
-      <header className="header">
-        <nav className="site-navigation">
-          <ul className="site-navigation__list">
+      <header className={`header ${stickyHeader ? 'header--sticky' : ''}`} ref={headerRef.current}>
+        <nav className={`site-navigation ${stickyNavigation ? 'site-navigation--sticky' : ''}`}>
+          <ul className={`site-navigation__list ${stickyNavigationList ? 'site-navigation__list--sticky' : ''}`}>
             <li className="site-navigation__item">
-              <NavLink to="/about" className="site-navigation__link">About</NavLink>
+              <NavLink to="/about" className={`site-navigation__link ${stickyNavigationLink ? 'site-navigation__link--sticky' : ''}`}>About</NavLink>
             </li>
             <li className="site-navigation__item">
-              <NavLink to="/volunteering" href="www.sd.com" className="site-navigation__link">Volunteering with us</NavLink>
+              <NavLink to="/volunteering" href="www.sd.com" className={`site-navigation__link ${stickyNavigationLink ? 'site-navigation__link--sticky' : ''}`}>Volunteering with us</NavLink>
             </li>
             <li className="site-navigation__item">
-              <NavLink to="/" className="site-navigation__link">
+              <NavLink to="/" className={`site-navigation__link ${stickyNavigationLink ? 'site-navigation__link--sticky' : ''}`}>
                 <img src={braustintx}
-                  className="site-navigation__logo"
+                  className={`site-navigation__logo ${stickyLogo ? 'site-navigation__logo--sticky' : ''}`}
                   alt="Bridges International Austin"
-                  title="Bridges International Austin"/>
+                  title="Bridges International Austin" />
               </NavLink>
             </li>
             <li className="site-navigation__item">
-              <NavLink to="/events" className="site-navigation__link">News & Events</NavLink>
+              <NavLink to="/events" className={`site-navigation__link ${stickyNavigationLink ? 'site-navigation__link--sticky' : ''}`}>News & Events</NavLink>
             </li>
             <li className="site-navigation__item">
-              <NavLink to="/getinvolved" className="site-navigation__link">Get Involved as An Student</NavLink>
+              <NavLink to="/getinvolved" className={`site-navigation__link ${stickyNavigationLink ? 'site-navigation__link--sticky' : ''}`}>Get Involved as An Student</NavLink>
             </li>
           </ul>
         </nav>
@@ -40,9 +90,9 @@ function App() {
       </Switch>
       <footer className="footer">
         <div className="footer__arrow">
-          <a href="#" id="buttonup" className="footer__arrow-link">
+          <Link to="#" id="buttonup" className="footer__arrow-link" onClick={ev => window.scroll({ top: 0, behavior: 'smooth' })}>
             <i className="fas fa-angle-up footer__arrow-up"></i>
-          </a>
+          </Link>
         </div>
         <ul className="footer__social">
           <li className="footer__item">
